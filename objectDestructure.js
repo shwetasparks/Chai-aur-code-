9231a42b0a27c3b8ed6ce774fcf12ec0b5d11c26
Shweta <shwetakoffficial@gmail.com>
2024-04-03 20:00:59 +0530

    object destructure

---
+++ objectDestructure.js
@@ -0,0 +1,27 @@
+//destructure
+const course = {
+   courseName: "js developer",
+   courseTeacher: "hitesh",
+   courseDescription: "this is javascript in hindi course",
+   courseDuration: "12 months",
+   coursePrice: "1000",
+};
+const { courseName: cName } = course; //can change key name
+console.log(cName);
+
+//{}=> destructuring
+
+//json data
+// {
+//    name: "shweta",
+//    hobby: "photography",
+//    city: "tokyo",
+// };
+
+
+[
+    {},
+    {},
+    {}
+]
+//jsonformatter-> tree
