839d686373b523924f4b92d62ace18163d8d05d3
Shweta <shwetakoffficial@gmail.com>
2024-04-04 11:15:04 +0530

    understanding scope in function ,hoisting

--- scope.js
+++ scope.js
@@ -1,16 +1,57 @@
-//global scope
-let a=450;
+// //global scope
+// let a=450;
+
+// if (true) {
+//     //block scope
+//    let a = 34;
+//    const b = 44;
+//    var c = 12;
+//    console.log("block scope : inner=",a)
+
+// }
+
+// console.log("global scope a = ",a);
+// console.log(b);
+// console.log(c);
+
+//nested function
+
+function one() {
+   const username = "shweta";
+   function two() {
+      const website = "youtube";
+      console.log(username);
+   }
+   // console.log(website)
+   two();
+}
+one();
 
 
-if (true) {
-    //block scope
-   let a = 34;
-   const b = 44;
-   var c = 12;  
-   console.log("block scope : inner=",a) 
-   
+//if else scope
+
+if(true){
+    const username = "shweta";
+    if(username==="shweta"){
+        const website="youtube"
+        console.log(username+" "+ website)
+    }
+    // console.log(website) //error out of scope
+}
+// console.log(username) //outof scope
+
+
+//*********  interesting */
+console.log(addone(4));
+function addone(num){
+    return num+1;
 }
 
-console.log("global scope a = ",a);
-console.log(b);
-console.log(c);
+
+//understanding hoisting
+//expression
+console.log(addTwo(6));  //can't declare function expression before declaration
+const addTwo= function (num){
+    return num+2;
+
+}
