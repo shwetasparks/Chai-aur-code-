2932f9b2b77cab92e5c9bda9653eb2381e87f973
Shweta <shwetakoffficial@gmail.com>
2024-04-04 18:38:01 +0530

    while and do while loop

---
+++ whileLoop.js
@@ -0,0 +1,20 @@
+//while loop
+let i = 0;
+while (i < 10) {
+   console.log(`value of index is ${i}`);
+   i = i + 2;
+}
+
+let array = 0;
+while (arr < array.length) {
+   console.log(array[arr]);
+   arr = arr + 1;
+}
+
+//do while loop
+
+let score = 3;
+do {
+   console.log(`score is ${score}`);
+   score++;
+} while (score <= 10);
