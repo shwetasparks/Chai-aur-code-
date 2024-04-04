bc30693d6c1ddbc77667f0459c2f17d22a24b793
Shweta <shwetakoffficial@gmail.com>
2024-04-04 16:51:45 +0530

    control flow

---
+++ controlFlow.js
@@ -0,0 +1,46 @@
+//if
+// const isUserLoggedIn = true;
+// if (isUserLoggedIn) {
+// }
+
+// 3!=2
+// <,<=,>=,==,!= ,!= ,=== ,!==
+
+// {}-> scope
+
+// const balance = 1000;
+// if (balance > 300) console.log("test");
+
+// if(balance<500){
+//     console.log("test");
+// } else(balance<900){
+//     console.log("less than 900");
+// };
+
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromEmail = false;
+
+//multiple condition : both condition must true
+// if (userLoggedIn && debitCard && (2 == 3)) {
+//    console.log("allowed to buy course");
+
+// const userLoggedIn= true
+
+//anyone should true
+// if(loggedInFromEmail||loggedInFromGoogle){
+//     console.log("user is logged in");
+// }
+
+/*********** switch case *********** */
+const month = 3;
+switch (month) {
+   case "jan":
+      console.log("jan");
+      break;
+   case "feb":
+      console.log("feb");
+      break;
+   default:
+      console.log("april");
+}
