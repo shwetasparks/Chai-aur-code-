1fe5042a8f5740c870bb1aafd07badce6662092d
Shweta <shwetakoffficial@gmail.com>
2024-04-05 02:37:32 +0530

    basic dom intro

---
+++ dom/script.js
@@ -0,0 +1,13 @@
+
+
+//window ->document
+//           /\
+//       head  body 
+//      /\       /\
+// title  meta  div -> attribute     
+//          /\
+//        h1  p
+
+//html collection
+//node collection
+//they are not array
