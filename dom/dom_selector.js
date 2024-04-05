10fb3259efb28a29a6025ee687436a1ecf8d2cd1
Shweta <shwetakoffficial@gmail.com>
2024-04-05 13:15:14 +0530

    dom selector

---
+++ dom/dom_selector.js
@@ -0,0 +1,19 @@
+//set attribute overright so right both 
+
+
+//attributes
+
+
+
+// title.innerText
+// 'Dom manipulation'
+// title.textContent : also show hidden text
+// 'Dom manipulation with JavaScript '
+
+//innerHtml- show complete html
+
+//Array.from()==> change into array
+
+
+// ********** note: practiced most of its code in browser only
+
