0c548ec062c415d04fcf126b7fa10496f68fe7bd
Shweta <shwetakoffficial@gmail.com>
2024-04-06 01:15:04 +0530

    guess number

---
+++ projects/Guess the number/script.js
@@ -0,0 +1,9 @@
+const randomNum = console.log(parseInt(Math.random() * 100 + 1));
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guess = document.querySelector(".guess");
+const remaining = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHigh");
+const startOver = document.querySelector(".resultParas");
+
+
