fd9181164fc3a84ec7a04b88ee66c6984df3b875
Shweta <shwetakoffficial@gmail.com>
2024-04-05 23:53:20 +0530

    Local Clock

---
+++ projects/digitalClock/script.js
@@ -0,0 +1,6 @@
+let clock = document.getElementById("clock");
+setInterval(function () {
+   let date = new Date();
+   //    console.log(date.toLocaleTimeString());
+   clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
