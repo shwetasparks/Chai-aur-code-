406b715b3cc84b0e6a4104f667f34edae59e53c6
Shweta <shwetakoffficial@gmail.com>
2024-04-07 15:46:12 +0530

    inheritence in js

---
+++ oops/inheritence.js
@@ -0,0 +1,28 @@
+class User {
+   constructor(username) {
+      this.username = username;
+   }
+
+   //
+   logMe() {
+      console.log(this.username);
+   }
+}
+
+class Teacher extends User {
+   constructor(username, email, password) {
+      super(username);
+      this.email = email;
+      this.password = password;
+   }
+   addCourse() {
+      console.log(this.username);
+   }
+}
+
+const chai = new Teacher("chai", "chai@gmail.com", "123");
+chai.addCourse();
+
+const masalaChai= new User("masala chai")
+masalaChai.logMe()
+
